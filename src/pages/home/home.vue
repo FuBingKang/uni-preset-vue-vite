5f217d46cb17faa67b3ef9af4b251e1cba488a4f
FuBingKang <fubingkang1029>
2025-12-24 14:00:59 +0800

    feat: 重构首页布局，添加粒子动画效果

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,381 +1,204 @@
 <template>
-  <div class="wrap">
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
-    <div class="c"></div>
+  <div class="page-bg"></div>
+
+  <div class="animation-wrapper">
+    <div class="particle particle-1"></div>
+    <div class="particle particle-2"></div>
+    <div class="particle particle-3"></div>
+    <div class="particle particle-4"></div>
+  </div>
+
+  <div class="page-wrapper">
+    <h4>CSS Particles</h4>
   </div>
 </template>
 
 <script></script>
 
 <style scoped lang="scss">
-// best in chrome
-// best in chrome
-$total: 300; // total particles
-$orb-size: 100px;
-$particle-size: 2px;
-$time: 8.5s;
-$base-hue: 0; // change for diff colors (180 is nice)
+/* Config */
+$color-bg: #1e1e1e;
+$color-particle: #fff;
+$spacing: 2560px;
+$time-1: 60s;
+$time-2: 120s;
+$time-3: 180s;
+$time-4: 200s;
 
+/* mixins */
+@function particles($max) {
+  $val: 0px 0px $color-particle;
+  @for $i from 1 through $max {
+    $val: #{$val}, random($spacing) + px random($spacing) + px $color-particle;
+  }
+  @return $val;
+}
+
+@mixin particles($max) {
+  box-shadow: particles($max);
+  border-radius: 50%;
+}
+
+/* Styles */
 html,
 body {
   height: 100%;
 }
 
 body {
-  background: black;
-  overflow: hidden; // no scrollbars..
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 4rem;
+  overflow: hidden;
 }
 
-.wrap {
+.page-bg,
+.animation-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.page-bg {
+  background: $color-bg;
+  background-blend-mode: screen;
+  background-size: cover;
+  filter: grayscale(100%);
+  z-index: -1;
+}
+
+.particle,
+.particle:after {
+  background: transparent;
+}
+
+.particle:after {
   position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 0;
-  height: 0;
-  transform-style: preserve-3d;
-  perspective: 1000px;
-  animation: rotate $time infinite linear; // rotate orb
+  content: "";
+  top: $spacing;
 }
 
-@keyframes rotate {
-  100% {
-    transform: rotateY(360deg) rotateX(360deg);
-  }
+.particle-1 {
+  animation: animParticle $time-1 linear infinite;
+  @include particles(1000);
+  height: 2px;
+  width: 2px;
 }
 
-.c {
-  position: absolute;
-  width: $particle-size;
-  height: $particle-size;
+.particle-1:after {
+  @include particles(900);
+  height: 2px;
+  width: 2px;
+}
+
+.particle-2 {
+  animation: animParticle $time-2 linear infinite;
+  @include particles(800);
+  height: 2px;
+  width: 2px;
+}
+
+.particle-2:after {
+  @include particles(500);
+  height: 3px;
+  width: 3px;
+}
+
+.particle-3:after {
+  @include particles(1100);
+  height: 3px;
+  width: 3px;
   border-radius: 50%;
-  opacity: 0;
 }
 
-@for $i from 1 through $total {
-  $z: (random(360) * 1deg); // random angle to rotateZ
-  $y: (random(360) * 1deg); // random to rotateX
-  $hue: ((40 / $total * $i) + $base-hue); // set hue
+.particle-4 {
+  animation: animParticle $time-4 linear infinite;
+  @include particles(1200);
+  height: 1px;
+  width: 1px;
+}
 
-  .c:nth-child(#{$i}) {
-    // grab the nth particle
-    animation: orbit#{$i} $time infinite;
-    animation-delay: ($i * 0.01s);
-    background-color: hsla($hue, 100%, 50%, 1);
+.particle-4:after {
+  @include particles(700);
+  height: 1px;
+  width: 1px;
+}
+
+@keyframes animParticle {
+  from {
+    transform: translateY(0px);
   }
+  to {
+    transform: translateY($spacing * -1);
+  }
+}
 
-  @keyframes orbit#{$i} {
-    20% {
-      opacity: 1; // fade in
-    }
-    30% {
-      transform: rotateZ(-$z) rotateY($y) translateX($orb-size) rotateZ($z); // form orb
-    }
-    80% {
-      transform: rotateZ(-$z) rotateY($y) translateX($orb-size) rotateZ($z); // hold orb state 30-80
-      opacity: 1; // hold opacity 20-80
-    }
-    100% {
-      transform: rotateZ(-$z) rotateY($y) translateX(($orb-size * 3))
-        rotateZ($z); // translateX * 3
-    }
+.page-wrapper {
+  text-align: center;
+  color: #fff;
+  z-index: 2;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h4 {
+  font-size:25px;
+  color: #fff;
+  font-family: Monoton;
+  -webkit-animation: neon1 1.5s ease-in-out infinite alternate;
+  -moz-animation: neon1 1.5s ease-in-out infinite alternate;
+  animation: neon1 1.5s ease-in-out infinite alternate;
+}
+
+h4:hover {
+  color: #ff1177;
+  -webkit-animation: none;
+  -moz-animation: none;
+  animation: none;
+}
+
+/*glow for webkit*/
+
+@-webkit-keyframes neon1 {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #ff1177,
+      0 0 70px #ff1177, 0 0 80px #ff1177, 0 0 100px #ff1177, 0 0 150px #ff1177;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff1177,
+      0 0 35px #ff1177, 0 0 40px #ff1177, 0 0 50px #ff1177, 0 0 75px #ff1177;
+  }
+}
+
+/*glow for mozilla*/
+
+@-moz-keyframes neon1 {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #ff1177,
+      0 0 70px #ff1177, 0 0 80px #ff1177, 0 0 100px #ff1177, 0 0 150px #ff1177;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff1177,
+      0 0 35px #ff1177, 0 0 40px #ff1177, 0 0 50px #ff1177, 0 0 75px #ff1177;
+  }
+}
+
+/*glow*/
+
+@keyframes neon1 {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #ff1177,
+      0 0 70px #ff1177, 0 0 80px #ff1177, 0 0 100px #ff1177, 0 0 150px #ff1177;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #ff1177,
+      0 0 35px #ff1177, 0 0 40px #ff1177, 0 0 50px #ff1177, 0 0 75px #ff1177;
   }
 }
 </style>
